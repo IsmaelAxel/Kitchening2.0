d407ca2e9f871e20d3f47e1eca461f66e5a207ae
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-10-05 12:18:05 -0300

    cambios en la base datos

---
+++ src/database/migrations/20231005151024-change-column-deleteAt-images.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.renameColumn('images', 'deleteAt', 'deletedAt');
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.renameColumn('images', 'deletedAt', 'deleteAt');
+  }
+};
